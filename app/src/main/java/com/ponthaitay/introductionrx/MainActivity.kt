18476a0e2285712f6db6aeba3be223dd73e0ea4e
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-13 20:13:58 +0700

    remove newline.

--- app/src/main/java/com/ponthaitay/introductionrx/MainActivity.kt
+++ app/src/main/java/com/ponthaitay/introductionrx/MainActivity.kt
@@ -19,7 +19,6 @@ import java.util.concurrent.TimeUnit
 class MainActivity : AppCompatActivity() {
 
     val TAG = javaClass.simpleName!!
-
     val listTest = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     override fun onCreate(savedInstanceState: Bundle?) {
